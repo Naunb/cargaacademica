4a3728e5f30314e28d2f5dff63fe26c3e9722234
dell <dell@DESKTOP-GR6JCN0>
2019-03-03 13:00:14 -0600

    Agregamos una lista de asignaturas

---
+++ cargaacademicaInformatica/cargaacademicaInformatica.Web/Controllers/AsignaturasController.cs
@@ -0,0 +1,40 @@
+﻿using cargaacademicaInformatica.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cargaacademicaInformatica.Web.Controllers
+{
+    public class AsignaturasController : Controller
+    {
+        // GET: Asignaturas
+        public ActionResult Index()
+        {
+            var asignaturas = new Asignaturas();
+            asignaturas.Id=0;
+            asignaturas.Descripcion = "Asignaturas Disponibles";
+
+            var asignaturas1 = new Asignaturas();
+            asignaturas1.Id = 1;
+            asignaturas1.Descripcion = "Lenguaje4";
+         
+            var asignaturas2 = new Asignaturas();
+            asignaturas2.Id = 2;
+            asignaturas2.Descripcion = "Taller de Hardware";
+
+            var asignaturas3 = new Asignaturas();
+            asignaturas3.Id = 3;
+            asignaturas3.Descripcion = "Teoria de Sistema";
+
+            var listadeAsignaturas = new List<Asignaturas>();
+            listadeAsignaturas.Add(asignaturas);
+            listadeAsignaturas.Add(asignaturas1);
+            listadeAsignaturas.Add(asignaturas2);
+            listadeAsignaturas.Add(asignaturas3);
+
+            return View(listadeAsignaturas);
+        }
+    }
+}
---
+++ cargaacademicaInformatica/cargaacademicaInformatica.Web/Models/Asignaturas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cargaacademicaInformatica.Web.Models
+{
+    public class Asignaturas
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
+
+
